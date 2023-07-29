f48a1a9e1978a7705f0a8efb166694e60721c606
Gaspar Ahumada <ahumadagaspar@gmail.com>
2023-07-29 18:35:59 -0300

    subiendo primer y segundo proyecto de python

---
+++ primerpy.py
@@ -0,0 +1,7 @@
+#Definir valor de X
+X = 10
+#Definir valor de Y
+Y = 5
+#Establecer la operaciòn
+resta = X - Y
+print(resta)
---
+++ segundopy.py
@@ -0,0 +1,6 @@
+pepito = 20
+julito = 13
+if (pepito > julito):
+    print ("julito es mayor que pepito porque tiene", pepito, "años")
+else:
+    print ("julito es menor que pepito porque tiene", pepito, "años")
