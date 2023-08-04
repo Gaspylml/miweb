387aa1f42b7eea8ef971b8b27df46734f98f694d
Gaspar Ahumada <ahumadagaspar@gmail.com>
2023-08-04 19:22:33 -0300

    subiendo archivos java y c++

---
+++ HolaMundo/src/main/java/proyecto1/holamundo/HolaMundo.java
@@ -0,0 +1,10 @@
+package proyecto1.holamundo;
+
+public class HolaMundo {
+
+    public static void main(String args[]){
+        System.out.println("Hola Mundo");
+        System.out.println("Segunda linea de codigo");
+    }
+    
+}
