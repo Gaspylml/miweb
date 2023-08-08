0cea3098a449cca4a616d70343f9e054786fdcfb
Gaspar Ahumada <ahumadagaspar@gmail.com>
2023-08-08 20:14:45 -0300

    añadiendo cambios de archivos HTML y C++

--- HolamundoC++.cpp
+++ HolamundoC++.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 int main() {
-std::cout << "Hola mundo" << std::endl;
-return 0;
+    std::cout << "Hola mundo" << std::endl;
+    std::cout << "Segunda linea de codigo" <<std::endl;
+    std::cout << "Tercer linea de codigo" <<std::endl;
 }
